5a772ff3338c6fc7e1abab4297d41881b3c6272d
grantlouisales <72000399+grantlouisales@users.noreply.github.com>
2021-10-11 09:47:46 -0600

    added the ground and the character

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 from ursina import *
+from ursina.prefabs.first_person_controller import FirstPersonController
 
 app = Ursina()
+player = FirstPersonController()
 
+
+class Voxel(Button):
+    def __init__(self, position=(0,0,0)):
+        super().__init__(
+            parent = scene,
+            position = position,
+            model = 'cube',
+            origin_y = .5,
+            texture = 'white_cube',
+            color = color.color(0, 0, random.uniform(.9, 1.0)),
+            highlight_color = color.lime,
+        )
+
+for z in range(100):
+    for x in range(3):
+        voxel = Voxel(position=(x,0,z))
 
 
 app.run()

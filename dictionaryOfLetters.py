d08e74a3acbd4dff2d81724f910dba640b9ca97a
grantlouisales <72000399+grantlouisales@users.noreply.github.com>
2021-10-25 09:19:11 -0600

    added changes to letter dictionary

--- dictionaryOfLetters.py
+++ dictionaryOfLetters.py
@@ -1,7 +1,17 @@
 """
 Purpose: Dictionary that holds letters and the corresponding pictures urls. 
 """
-letter_dictionary = {"a": 1, "b": 2, "c": 3, "d": 4, "e": 5, "f": 6, "g": 7, "h": 8,
-                     "i": 9, "j": 10, "k": 11, "l": 12, "m": 13, "n": 14, "o": 15,
-                     "p": 16, "q": 17, "r": 18, "s": 19, "t": 20, "u": 21, "v": 22,
-                     "w": 23, "x": 24, "y": 25, "z": 26}
+from PIL import Image
+
+letter_dictionary = {"a": "resources\letters\A_gold.png", "b": "resources\letters\B_gold.png", "c": "resources\letters\C_gold.png",
+                     "d": "resources\letters\D_gold.png", "e": "resources\letters\E_gold.png", "f": "resources\letters\F_gold.png",
+                     "g": "resources\letters\G_gold.png", "h": "resources\letters\H_gold.png", "i": "resources\letters\I_gold.png",
+                     "j": "resources\letters\J_gold.png", "k": "resources\letters\K_gold.png", "l": "resources\letters\L_gold.png",
+                     "m": "resources\letters\M_gold.png", "n": "resources\letters\letterN_gold.png", "o": "resources\letters\O_gold.png",
+                     "p": "resources\letters\P_gold.png", "q": "resources\letters\Q_gold.png", "r": "resources\letters\R_gold.png",
+                     "s": "resources\letters\S_gold.png", "t": "resources\letters\T_gold.png", "u": "resources\letters\letterU_gold.png",
+                     "v": "resources\letters\V_gold.png", "w": "resources\letters\W_gold.png", "x": "resources\letters\X_gold.png",
+                     "y": "resources\letters\Y_gold.png", "z": "resources\letters\Z_gold.png"}
+
+im = Image.open(letter_dictionary["u"])
+im.show()

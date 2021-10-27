2e6cb0c928f963e546e5803be5dc8ca48804a06c
Rhys Benson <bensonrhys702@gmail.com>
2021-10-27 10:44:53 -0600

    Resized images and removed transparency.

--- dictionaryOfLetters.py
+++ dictionaryOfLetters.py
@@ -3,15 +3,15 @@ Purpose: Dictionary that holds letters and the corresponding pictures urls.
 """
 from PIL import Image
 
-letter_dictionary = {"a": "resources\letters\A_gold.png", "b": "resources\letters\B_gold.png", "c": "resources\letters\C_gold.png",
-                     "d": "resources\letters\D_gold.png", "e": "resources\letters\E_gold.png", "f": "resources\letters\F_gold.png",
-                     "g": "resources\letters\G_gold.png", "h": "resources\letters\H_gold.png", "i": "resources\letters\I_gold.png",
-                     "j": "resources\letters\J_gold.png", "k": "resources\letters\K_gold.png", "l": "resources\letters\L_gold.png",
-                     "m": "resources\letters\M_gold.png", "n": "resources\letters\letterN_gold.png", "o": "resources\letters\O_gold.png",
-                     "p": "resources\letters\P_gold.png", "q": "resources\letters\Q_gold.png", "r": "resources\letters\R_gold.png",
-                     "s": "resources\letters\S_gold.png", "t": "resources\letters\T_gold.png", "u": "resources\letters\letterU_gold.png",
-                     "v": "resources\letters\V_gold.png", "w": "resources\letters\W_gold.png", "x": "resources\letters\X_gold.png",
-                     "y": "resources\letters\Y_gold.png", "z": "resources\letters\Z_gold.png"}
+letter_dictionary = {"a": "resources\letters\letterA.png", "b": "resources\letters\letterB.png", "c": "resources\letters\letterC.png",
+                     "d": "resources\letters\letterD.png", "e": "resources\letters\letterE.png", "f": "resources\letters\letterF.png",
+                     "g": "resources\letters\letterG.png", "h": "resources\letters\letterH.png", "i": "resources\letters\letterI.png",
+                     "j": "resources\letters\letterJ.png", "k": "resources\letters\letterK.png", "l": "resources\letters\letterL.png",
+                     "m": "resources\letters\letterM.png", "n": "resources\letters\letterN.png", "o": "resources\letters\letterO.png",
+                     "p": "resources\letters\letterP.png", "q": "resources\letters\letterQ.png", "r": "resources\letters\letterR.png",
+                     "s": "resources\letters\letterS.png", "t": "resources\letters\letterT.png", "u": "resources\letters\letterU.png",
+                     "v": "resources\letters\letterV.png", "w": "resources\letters\letterW.png", "x": "resources\letters\letterX.png",
+                     "y": "resources\letters\letterY.png", "z": "resources\letters\letterZ.png"}
 
 im = Image.open(letter_dictionary["u"])
 im.show()

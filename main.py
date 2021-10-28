8033eca31cc11b324bfdbc0fccdbebaa96252ac1
Grant Ales <grant.ales@yahoo.com>
2021-10-27 19:40:51 -0600

    changes

--- main.py
+++ main.py
@@ -6,18 +6,20 @@ from ursina.mesh_importer  import *
 from dictionaryOfLetters import letter_dictionary
 
 app = Ursina()
-player = FirstPersonController(collider ='box', speed = 10, origin = (-72,-72,0))
+player = FirstPersonController(collider = 'box', speed = 10, origin = (-72,-72,0), scale=0.25)
 
 window.fps_counter.enabled = False
 
 model1 = load_model("table v1.obj")
 table = Entity(model= model1,
-                origin = (100, 100, -100),
+                origin = (0,0,0),
                 texture = 'white_cube',
-                scale = 0.25,
-                color = color.black)
+                scale = .0125,
+                color = color.green,
+                rotation = Vec3(-90,90,0))
 
-table.collider = MeshCollider(table, mesh=table.model, center=Vec3(100,100,-100))
+table.collider = MeshCollider(table, mesh=table.model, center=Vec3(0,0,0))
+table.collider.visible = True
 
 #camera.parent = player
 

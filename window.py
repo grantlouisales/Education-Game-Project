56dca1817a624433511dd2cd806fc92fa9d9e254
Grant Ales <grant.ales@yahoo.com>
2021-11-04 11:24:20 -0600

    added better keyboard response

--- window.py
+++ window.py
@@ -1,50 +1,94 @@
 import arcade
 
+MOVEMENT_SPEED = 5
+
+class Player(arcade.Sprite):
+    
+
+    def update(self):
+        self.center_x += self.change_x
+        self.center_y += self.change_y
+
+        if self.left < 0:
+            self.left = 0
+        elif self.right > 1280 - 1:
+            self.right = 1280 - 1
+
+        if self.bottom < 0:
+            self.bottom = 0
+        elif self.top > 720 - 1:
+            self.top = 720 - 1
+
 class MyGameWindow(arcade.Window):
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
-        self.set_location(400,200)
 
         arcade.set_background_color(arcade.color.AERO_BLUE)
 
-        self.player_x = width / 2
-        self.player_y = height / 2
-        self.player_speed = 250
+        # Variables that will hold sprite list
+        self.player_list = None
 
-        self.sprite1 = arcade.Sprite(":resources:images/space_shooter/playerShip1_green.png")
+        # Set up player info
+        self.player_sprite = None
 
+    def setup(self):
+        # Sprite List
+        self.player_list = arcade.SpriteList()
+
+        # Set up player
+        self.player_sprite = Player(":resources:images/animated_characters/female_person/femalePerson_idle.png", 1)
+
+        self.player_sprite.center_x = self.width / 2
+        self.player_sprite.center_y = self.height / 2
+        self.player_list.append(self.player_sprite)
 
     def on_draw(self):
+        """
+        Render the screen.
+        """
+
+        # This command has to happen before we start drawing
         arcade.start_render()
-        self.sprite1.draw()
-        
+
+        # Draw all the sprites.
+        self.player_list.draw()
 
     def on_update(self, delta_time):
-        if self.player_y > 720:
-            self.player_y = 10
-        if self.player_y < 0:
-            self.player_y = 720
-        if self.player_x > 1280:
-            self.player_x = 10
-        if self.player_x < 0:
-            self.player_x = 1270
+        """ Movement and game logic """
 
-        self.sprite1.set_position(self.player_x, self.player_y)
+        # Move the player
+        self.player_list.update()
 
-
-    def on_key_press(self, symbol, modifiers):
-        if symbol == arcade.key.D:
-            self.player_x += 50
-        if symbol == arcade.key.A:
-            self.player_x -= 50
-        if symbol == arcade.key.W:
-            self.player_y += 50
-        if symbol == arcade.key.S:
-            self.player_y -= 50
+    def on_key_press(self, key, modifiers):
+        # If the player presses a key, update the speed
+        if key == arcade.key.W:
+            self.player_sprite.change_y = MOVEMENT_SPEED
+        elif key == arcade.key.S:
+            self.player_sprite.change_y = -MOVEMENT_SPEED
+        elif key == arcade.key.A:
+            self.player_sprite.change_x = -MOVEMENT_SPEED
+        elif key == arcade.key.D:
+            self.player_sprite.change_x = MOVEMENT_SPEED
 
 
+    def on_key_release(self, key, modifiers):
+        """Called when the user releases a key. """
+
+        # If a player releases a key, zero out the speed.
+        # This doesn't work well if multiple keys are pressed.
+        # Use 'better move by keyboard' example if you need to
+        # handle this.
+        if key == arcade.key.W or key == arcade.key.S:
+            self.player_sprite.change_y = 0
+        elif key == arcade.key.A or key == arcade.key.D:
+            self.player_sprite.change_x = 0
         
-        
 
-MyGameWindow(1280, 720, "My Game Window")
-arcade.run()
+def main():
+    window = MyGameWindow(1280, 720, "My Game Window")
+    window.setup()
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()

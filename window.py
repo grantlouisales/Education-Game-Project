56779cc60eafae7157916f972849a32fcd00f223
Grant Ales <grant.ales@yahoo.com>
2021-11-03 20:26:49 -0600

    changed key presses to wasd

--- window.py
+++ window.py
@@ -7,40 +7,33 @@ class MyGameWindow(arcade.Window):
 
         arcade.set_background_color(arcade.color.AERO_BLUE)
 
-        self.x = 100
-        self.y = 100
-        self.x_speed = 300
-        self.y_speed = 150
+        self.player_x = width / 2
+        self.player_y = height / 2
+        self.player_speed = 250
 
-        self.player_x = 100
-        self.player_y = 100
+        self.sprite1 = arcade.Sprite(":resources:images/space_shooter/playerShip1_green.png")
+
 
     def on_draw(self):
         arcade.start_render()
-        arcade.draw_circle_filled(self.x, self.y, 60, arcade.color.RED_DEVIL, 10)
-        arcade.draw_circle_outline(self.player_x, self.player_y, 100, arcade.color.RED_DEVIL, 10)
-
+        self.sprite1.draw()
+        
 
     def on_update(self, delta_time):
-        # Delta time is 1/60 = 0.01666 * 300 = 5 pixels
-        self.x += self.x_speed * delta_time
-        self.y += self.y_speed * delta_time
+        self.sprite1.set_position(self.player_x, self.player_y)
 
-        if self.x > 1280 or self.x < 0:
-            self.x_speed *= -1
-
-        if self.y > 720 or self.y < 0:
-            self.y_speed *= -1
 
     def on_key_press(self, symbol, modifiers):
-        if symbol == arcade.key.RIGHT:
+        if symbol == arcade.key.D:
             self.player_x += 100
-        if symbol == arcade.key.LEFT:
+        if symbol == arcade.key.A:
             self.player_x -= 100
-        if symbol == arcade.key.UP:
+        if symbol == arcade.key.W:
             self.player_y += 100
-        if symbol == arcade.key.DOWN:
+        if symbol == arcade.key.S:
             self.player_y -= 100
+
+
         
         
 

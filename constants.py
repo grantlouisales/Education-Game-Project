3c3ff186ace7a2f59ec6cb5298a073d2755a815e
NickCampbell91 <nickcampbell1991@gmail.com>
2021-11-10 08:27:16 -0700

    Create constants.py
    Made file for our constants so all files can use them.

---
+++ constants.py
@@ -0,0 +1,15 @@
+# Constants
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 650
+SCREEN_TITLE = "Platformer"
+
+# Constants used to scale our sprites from their original size
+CHARACTER_SCALING = .5
+TILE_SCALING = 0.5
+SPRITE_PIXEL_SIZE = 128
+GRID_PIXEL_SIZE = SPRITE_PIXEL_SIZE * TILE_SCALING
+
+# Movement speed of player, in pixels per frame
+PLAYER_MOVEMENT_SPEED = 10
+GRAVITY = 1
+PLAYER_JUMP_SPEED = 20

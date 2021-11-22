a263f917dbfd9d7af96d5f424efef6b056bd1b26
Benjamin Witham <wit18005@byui.edu>
2021-11-22 10:03:30 -0700

    fix merge

---
+++ Tileset1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Tileset1" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
+ <image source="Tileset1.png" width="1024" height="1024"/>
+</tileset>
---
+++ terrain.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="terrain" tilewidth="32" tileheight="32" tilecount="247" columns="19">
+ <image source="terrain.png" width="608" height="416"/>
+</tileset>

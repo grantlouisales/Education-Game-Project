e2283a53488ea5ca9f35578a9afa595664f43575
Rhys Benson <bensonrhys702@gmail.com>
2021-12-08 09:59:46 -0700

    updated spelling class

--- spelling.py
+++ spelling.py
@@ -12,15 +12,12 @@ from constants import *
 from MenuView import *
 from read_words_file import *
 
-
 class Spelling():
     
     class Letter(arcade.Sprite):
         def __init__(self, letter, x, y):
             super().__init__(f'resources/letters/letter{str.upper(letter)}.png', center_x=x, center_y=y, scale=.1)
             self.letter = letter
-             
-
 
     def __init__(self, scene : Scene, player : Sprite, map : string):
         self.letters_collected = []
@@ -30,27 +27,24 @@ class Spelling():
         self.curr_letter = None
         self.player = player
         self.map = map
-        self.locations = self.get_locations()
-        self.correct_ping = arcade.sound.load_sound("audio/correct_ping.mp3")
-        self.error_ping = arcade.sound.load_sound("audio/error_ping.mp3")
+
+        self.set_difficulty()
         self.get_new_word()
         self.start_word()
 
-    def get_locations(self):
+    def set_difficulty(self):
         if self.map == "Map1Hard.json":
-            return HARD_LOCATIONS
+            self.locations = HARD_LOCATIONS
+            self.words_list = get_hard_words()
+            self.num_spawns = 5
         elif self.map == "Map1Medium.json":
-            return MED_LOCATIONS
+            self.locations = MED_LOCATIONS
+            self.words_list = get_medium_words()
+            self.num_spawns = 3
         else:
-            return EASY_LOCATIONS
-
-    def get_words_list(self):
-        if self.map == "Map1Hard.json":
-            return get_hard_words()
-        elif self.map == "Map1Medium.json":
-            return get_medium_words()
-        else:
-            return get_easy_words()
+            self.locations = EASY_LOCATIONS
+            self.words_list = get_easy_words()
+            self.num_spawns = 2
 
     def create_letter(self, letter : str, position):
         letter_sprite = self.Letter(letter, position[0], position[1])
@@ -58,15 +52,9 @@ class Spelling():
         self.scene.add_sprite(f'Letter', letter_sprite)
 
     def collect_letter(self, letter : Letter):
-        # Play sound Accordingly, depending on the letters collected!!!
-        # if letter.letter in self.curr_word:
-        #     arcade.play_sound(self.correct_ping)
-        # else:
-        #     arcade.play_sound(self.error_ping)
         self.letters_collected.append(letter)
-    
-       
         index = self.letters_collected.index(letter) + 1
+        
         self.clear_letters()
         self.next_letter(letter.position)
 
@@ -81,28 +69,19 @@ class Spelling():
         self.curr_letter = (self.curr_word[0], 0)
         self.generate_letters(self.locations, prev_location)
 
-    def get_new_word(self, old_word = None):
-        list_words = self.get_words_list()
-        test_word = random.choice(list_words)
-        if not test_word == old_word:
-            self.curr_word = test_word
-
+    def get_new_word(self):
+        self.curr_word = random.choice(self.words_list)
 
     def generate_letters(self, pos_list, prev_location):
         while True:
-            locations = random.sample(pos_list, 3)
-            for location in locations:
-                print(location)
+            locations = random.sample(pos_list, self.num_spawns)
             if not prev_location in locations:
                 break
-            else:
-                print("Failed")
 
-        letters = random.sample(string.ascii_lowercase, 2)
+        letters = random.sample(string.ascii_lowercase, self.num_spawns - 1)
         self.create_letter(self.curr_letter[0], locations.pop())
         for place in locations:
             self.create_letter(letters.pop(), place)
-        print("--------------------------")
 
     def clear_letters(self):
         self.scene.remove_sprite_list_by_name('Letter')
@@ -112,11 +91,7 @@ class Spelling():
         index = self.curr_letter[1]
         if index == len(self.curr_word) - 1:
             if self.assemble_word() == self.curr_word:
-                arcade.play_sound(self.correct_ping)
-                self.get_new_word(old_word=self.curr_word) 
-            else:
-                arcade.play_sound(self.error_ping)
-                
+                self.get_new_word()            
             self.start_word(prev_location=prev_location)
             self.draw_word = True
             self.word_timer = 0 
